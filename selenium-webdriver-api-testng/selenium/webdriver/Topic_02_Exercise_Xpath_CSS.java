4dbd82ec1446ca1fe0d79ad553c13c3fcc99ff1d
PhuongTTD <tranphuong.thkt@gmail.com>
2020-09-27 22:14:05 +0700

    Exercise xpath and css - create TC 03-04

--- selenium-webdriver-api-testng/selenium/webdriver/Topic_02_Exercise_Xpath_CSS.java
+++ selenium-webdriver-api-testng/selenium/webdriver/Topic_02_Exercise_Xpath_CSS.java
@@ -1,11 +1,9 @@
 package webdriver;
 
 import org.testng.annotations.Test;
-
-import nl.flotsam.xeger.Xeger;
-
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -26,10 +24,10 @@ public class Topic_02_Exercise_Xpath_CSS {
 		// Click on link "My Account" to redirect on login page
 		driver.findElement(By.xpath(".//div[@class='footer']//a[(text()='My Account')]")).click();
 		// Empty username and pass
-		driver.findElement(By.xpath(".//input[@name='login[username]']")).sendKeys();
-		driver.findElement(By.xpath(".//input[@name='login[password]']")).sendKeys();
+		driver.findElement(By.name("login[username]")).sendKeys();
+		driver.findElement(By.name("login[password]")).sendKeys();
 		// Click login btn
-		driver.findElement(By.xpath(".//button[@name='send']")).click();
+		driver.findElement(By.name("send")).click();
 		// Verify error mess
 		WebElement emailalert = driver.findElement(By.id("advice-required-entry-email"));
 		String actual1 = emailalert.getText();
@@ -42,21 +40,43 @@ public class Topic_02_Exercise_Xpath_CSS {
 
 	@Test
 	public void TC_02() {
-		// Click on link "My Account" to redirect on login page
 		driver.findElement(By.xpath(".//div[@class='footer']//a[(text()='My Account')]")).click();
-		// Empty username and pass
 //		String generateUsername = getRandomStringWithRegex("[^$@gmail.com]\\{1,16}");
-		driver.findElement(By.xpath(".//input[@name='login[username]']")).sendKeys("12341234@1234.123");
-		driver.findElement(By.xpath(".//input[@name='login[password]']")).sendKeys("1242464");
-		// Click login btn
-		driver.findElement(By.xpath(".//button[@name='send']")).click();
-		// Verify error mess
+		driver.findElement(By.name("login[username]")).sendKeys("1242@23432.123");
+		driver.findElement(By.name("login[password]")).sendKeys("1242464");
+		driver.findElement(By.name("send")).click();
 		WebElement emailalert = driver.findElement(By.id("advice-validate-email-email"));
 		String actual1 = emailalert.getText();
 		String expected = "Please enter a valid email address. For example johndoe@domain.com.";
 		Assert.assertEquals(actual1, expected);
 	}
+	
+	@Test
+	public void TC_03() {
+		driver.findElement(By.xpath(".//div[@class='footer']//a[(text()='My Account')]")).click();
+		driver.findElement(By.name("login[username]")).sendKeys("automation@gmail.com");
+		driver.findElement(By.name("login[password]")).sendKeys("123");
+		driver.findElement(By.name("send")).click();
+//		WebElement emailalert = driver.findElement(By.id("advice-required-entry-email"));
+//		String actual1 = emailalert.getText();
+		WebElement passalert = driver.findElement(By.id("advice-validate-password-pass"));
+		String actual2 = passalert.getText();
+		String expected = "Please enter 6 or more characters without leading or trailing spaces.";
+//		Assert.assertEquals(actual1, expected);
+		Assert.assertEquals(actual2, expected);
+	}
 
+	@Test
+	public void TC_04() {
+		driver.findElement(By.xpath(".//div[@class='footer']//a[(text()='My Account')]")).click();
+		driver.findElement(By.name("login[username]")).sendKeys("automation@gmail.com");
+		driver.findElement(By.name("login[password]")).sendKeys("1243242343");
+		driver.findElement(By.name("send")).click();
+		WebElement emailalert = driver.findElement(By.xpath("//li[@class='error-msg']/child::*/li"));
+		String actual = emailalert.getText();
+		String expected = "Invalid login or password.";
+		Assert.assertEquals(actual, expected);
+	}
 	@AfterMethod
 	public void afterMethod() {
 		driver.quit();
